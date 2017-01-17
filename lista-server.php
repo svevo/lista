258b67dc5a0dcbd532c36f8fd474d9357923e520
s.samperi@gmail.com <s.samperi@gmail.com>
2017-01-17 12:18:15 +0100

    Primo commit

---
+++ lista-server.php
@@ -0,0 +1,25 @@
+[{
+        "item": "Anna"
+    }
+,{
+        "item": "Bartolo"
+    }
+,{
+        "item": "Carlo"
+    }
+,{
+        "item": "Diana"
+    }
+,{
+        "item": "Enzo"
+    }
+,{
+        "item": "Francesca"
+    }
+,{
+        "item": "Giorgio"
+    }
+,{
+        "item": "Ileana"
+    }
+]
